d36326b207e160d68255423024b3fd4d70bb1b23
Andrii Kulyk <andrij.kulyk@gmail.com>
2023-05-24 17:47:03 +0300

    Add nft/collection search

--- src/controllers/collection/getAll.js
+++ src/controllers/collection/getAll.js
@@ -1,6 +1,21 @@
 const {NftSet} = require('../../models');
 
 const getAll = async (req, res) => {
+    const {search} = req.query;
+    
+    if (search) {
+        const collections = await NftSet.find({title: {$regex: search, '$options': 'i'}})
+        .populate(
+            'nfts',
+            {'imageUrl': 1})
+        .populate(
+            'author',
+            {'name': 1, 'avatarUrl': 1}
+        );
+
+        return res.status(200).json(collections);
+    }
+
     const collections = await NftSet.find({}).populate(
         'nfts',
         {'imageUrl': 1}
--- src/controllers/nft/getAll.js
+++ src/controllers/nft/getAll.js
@@ -1,6 +1,19 @@
 const {Nft} = require('../../models');
 
 const getAll = async (req, res) => {
+    const {search} = req.query;
+
+    if (search) {
+        const nfts = await Nft.find({title: {$regex: search, '$options': 'i'}})
+        .populate(
+            'author',
+            {'name': 1, 'avatarUrl': 1}
+        )
+        .sort({createdAt: -1})
+
+        return res.status(200).json(nfts);  
+    };
+
     const nfts = await Nft.find({}).populate(
         'author',
         {'name': 1, 'avatarUrl': 1}
